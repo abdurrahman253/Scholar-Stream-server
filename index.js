2a683a11147281fcc88496812f7d33addf61642c
Abdur rahman <abrahman5676@gmail.com>
2026-01-06 19:43:05 +0600

    delete multer

--- index.js
+++ index.js
@@ -24,15 +24,6 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// Multer Storage config
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage: storage });
 
 // --- JWT Middleware ---
 const verifyJWT = async (req, res, next) => {
